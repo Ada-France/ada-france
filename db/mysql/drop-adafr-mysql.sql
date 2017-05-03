a71a7769ce21ab4d1575efde26cc0212ac2de022
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-05-03 22:04:59 +0200

    SQL files

---
+++ db/mysql/drop-adafr-mysql.sql
@@ -0,0 +1,48 @@
+/* Copied from awa-wikis-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_wiki_space`;
+DROP TABLE IF EXISTS `awa_wiki_page`;
+DROP TABLE IF EXISTS `awa_wiki_content`;
+/* Copied from awa-images-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_image`;
+/* Copied from awa-jobs-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_job`;
+/* Copied from awa-storages-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_store_local`;
+DROP TABLE IF EXISTS `awa_storage_folder`;
+DROP TABLE IF EXISTS `awa_storage_data`;
+DROP TABLE IF EXISTS `awa_storage`;
+/* Copied from awa-blogs-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_post`;
+DROP TABLE IF EXISTS `awa_blog`;
+/* Copied from awa_counters-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_visit`;
+DROP TABLE IF EXISTS `awa_counter_definition`;
+DROP TABLE IF EXISTS `awa_counter`;
+/* Copied from awa-tags-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_tagged_entity`;
+DROP TABLE IF EXISTS `awa_tag`;
+/* Copied from awa-comments-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_comment`;
+/* Copied from awa-workspaces-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_workspace_member`;
+DROP TABLE IF EXISTS `awa_workspace_feature`;
+DROP TABLE IF EXISTS `awa_workspace`;
+/* Copied from awa-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_user`;
+DROP TABLE IF EXISTS `awa_session`;
+DROP TABLE IF EXISTS `awa_email`;
+DROP TABLE IF EXISTS `awa_access_key`;
+DROP TABLE IF EXISTS `awa_acl`;
+DROP TABLE IF EXISTS `awa_queue`;
+DROP TABLE IF EXISTS `awa_message_type`;
+DROP TABLE IF EXISTS `awa_message`;
