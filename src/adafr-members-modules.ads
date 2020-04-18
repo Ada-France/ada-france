b3b772a640c3e509b8a24273ca515cca0cfbe76f
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-04-18 15:48:02 +0200

    Member registration support - Define the events to post email at various registration steps - Define the permissions to control the display and edition of member info - Define Save, Load, Send_Mail_Validation, Save_Payment

--- src/adafr-members-modules.ads
+++ src/adafr-members-modules.ads
@@ -15,13 +15,37 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+with Ada.Strings.Unbounded;
 with ASF.Applications;
 
+with ADO;
+with Security.Permissions;
 with AWA.Modules;
+with AWA.Events;
+with Adafr.Members.Models;
+private with Security.Random;
 package Adafr.Members.Modules is
+
+   package Send_Validate_Event is
+     new AWA.Events.Definition (Name => "member-send-validate");
+   package Send_Subscribed_Event is
+     new AWA.Events.Definition (Name => "member-subscribed");
+   package Send_Pending_Member_Event is
+     new AWA.Events.Definition (Name => "member-pending");
+   package Send_Registered_Member_Event is
+     new AWA.Events.Definition (Name => "member-registered");
+   package Send_Updated_Member_Event is
+     new AWA.Events.Definition (Name => "member-updated");
+
+   package ACL_View_Member is
+     new Security.Permissions.Definition ("member-view");
+   package ACL_Update_Member is
+     new Security.Permissions.Definition ("member-update");
 
    --  The name under which the module is registered.
    NAME : constant String := "members";
+
+   PARAM_SIGN_KEY : constant String := "secret_key";
 
    --  ------------------------------
    --  Module members
@@ -35,11 +59,62 @@ package Adafr.Members.Modules is
                          App    : in AWA.Modules.Application_Access;
                          Props  : in ASF.Applications.Config);
 
+   --  Configures the module after its initialization and after having read
+   --  its XML configuration.
+   overriding
+   procedure Configure (Plugin : in out Member_Module;
+                        Props  : in ASF.Applications.Config);
+
    --  Get the members module.
    function Get_Member_Module return Member_Module_Access;
 
+
+   --  Send_Mail_Validation
+   procedure Send_Mail_Validation (Model  : in out Member_Module;
+                                   Email  : in String;
+                                   Entity : in out Adafr.Members.Models.Member_Ref'Class);
+
+   --  Validate the secure key
+   procedure Validate_Key (Model  : in out Member_Module;
+                           Key    : in String;
+                           Entity : in out Adafr.Members.Models.Member_Ref'Class);
+
+   --  Load the member information.
+   procedure Load (Model   : in out Member_Module;
+                   Id      : in ADO.Identifier;
+                   Entity  : in out Adafr.Members.Models.Member_Ref'Class;
+                   History : in out Adafr.Members.Models.Audit_Info_List_Bean);
+
+   --  Save the member information after validating the secure key
+   procedure Save (Model  : in out Member_Module;
+                   Id     : in ADO.Identifier;
+                   Key    : in String;
+                   Member : in out Adafr.Members.Models.Member_Bean'Class);
+
+   --  Register the member for the Ada-France or Ada-France+Ada-Europe
+   --  after validating the secure key
+   procedure Register (Model  : in out Member_Module;
+                       Member : in out Adafr.Members.Models.Member_Bean'Class);
+
+   --  Update the status and payment information for the member.
+   procedure Save_Payment (Model  : in out Member_Module;
+                           Id     : in ADO.Identifier;
+                           Member : in out Adafr.Members.Models.Member_Bean'Class);
+
 private
 
-   type Member_Module is new AWA.Modules.Module with null record;
+   type Member_Module is new AWA.Modules.Module with record
+      Random      : Security.Random.Generator;
+      Sign_Key    : Ada.Strings.Unbounded.Unbounded_String;
+   end record;
+
+   function Get_Secure_Key (Model : in Member_Module;
+                            Member : in Adafr.Members.Models.Member_Ref'Class)
+                            return String;
+
+   function Validate_Secure_Key (Model  : in Member_Module;
+                                 Value  : in String;
+                                 Member : in Adafr.Members.Models.Member_Ref'Class)
+                                 return Boolean;
 
 end Adafr.Members.Modules;
