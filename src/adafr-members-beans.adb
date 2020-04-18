726dfa8ead81534bf30b18937727af0738f33c25
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-04-18 15:47:15 +0200

    Member list and registration - Implement the Member_Bean operations - Implement the Member_List_Bean operations

--- src/adafr-members-beans.adb
+++ src/adafr-members-beans.adb
@@ -15,26 +15,51 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-
-with ASF.Events.Faces.Actions;
+with ADO.Utils;
+with ADO.Objects;
+with ADO.Sessions;
+with ADO.Queries;
+with ASF.Contexts.Faces;
+with ASF.Contexts.Flash;
+with ASF.Applications.Messages.Factory;
+with AWA.Services.Contexts;
 package body Adafr.Members.Beans is
 
+   use ASF.Applications;
+   use Ada.Strings.Unbounded;
+   use type ADO.Identifier;
+
+   package ASC renames AWA.Services.Contexts;
+
+   procedure Invalid_Secure_Key (Outcome : out Ada.Strings.Unbounded.Unbounded_String);
+
    --  ------------------------------
-   --  Example of action method.
+   --  Add a message to the flash context so that it will be displayed on the next page.
    --  ------------------------------
-   procedure Action (Bean    : in out Member_Bean;
-                     Outcome : in out Ada.Strings.Unbounded.Unbounded_String) is
+   procedure Invalid_Secure_Key (Outcome : out Ada.Strings.Unbounded.Unbounded_String) is
+      Ctx   : constant ASF.Contexts.Faces.Faces_Context_Access := ASF.Contexts.Faces.Current;
+      Flash : constant ASF.Contexts.Faces.Flash_Context_Access := Ctx.Get_Flash;
    begin
-      null;
-   end Action;
+      Flash.Set_Keep_Messages (True);
+      Messages.Factory.Add_Message (Ctx.all, "members.secure_key_was_invalid_error",
+                                    Messages.ERROR);
+      Outcome := To_Unbounded_String ("failure");
+   end Invalid_Secure_Key;
 
-   package Action_Binding is
-     new ASF.Events.Faces.Actions.Action_Method.Bind (Bean   => Member_Bean,
-                                                      Method => Action,
-                                                      Name   => "action");
-
-   Member_Bean_Binding : aliased constant Util.Beans.Methods.Method_Binding_Array
-     := (Action_Binding.Proxy'Access, null);
+   --  ------------------------------
+   --  Set the bean attribute identified by the name.
+   --  ------------------------------
+   overriding
+   procedure Set_Value (Item  : in out Member_Bean;
+                        Name  : in String;
+                        Value : in Util.Beans.Objects.Object) is
+   begin
+      if Name = "id" and not Util.Beans.Objects.Is_Empty (Value) then
+         Item.Id := ADO.Utils.To_Identifier (Value);
+      else
+         Models.Member_Bean (Item).Set_Value (Name, Value);
+      end if;
+   end Set_Value;
 
    --  ------------------------------
    --  Get the value identified by the name.
@@ -43,36 +68,102 @@ package body Adafr.Members.Beans is
    function Get_Value (From : in Member_Bean;
                        Name : in String) return Util.Beans.Objects.Object is
    begin
-      if Name = "count" then
-         return Util.Beans.Objects.To_Object (From.Count);
+      if Name = "history" then
+         return Util.Beans.Objects.To_Object (Value   => From.History_Bean,
+                                              Storage => Util.Beans.Objects.STATIC);
+
       else
-         return Util.Beans.Objects.Null_Object;
+         return Models.Member_Bean (From).Get_Value (Name);
       end if;
    end Get_Value;
 
    --  ------------------------------
-   --  Set the value identified by the name.
+   --  Send the member email validation request.
    --  ------------------------------
    overriding
-   procedure Set_Value (From  : in out Member_Bean;
-                        Name  : in String;
-                        Value : in Util.Beans.Objects.Object) is
+   procedure Send (Bean    : in out Member_Bean;
+                   Outcome : in out Ada.Strings.Unbounded.Unbounded_String) is
    begin
-      if Name = "count" then
-         From.Count := Util.Beans.Objects.To_Integer (Value);
-      end if;
-   end Set_Value;
+      Bean.Module.Send_Mail_Validation (To_String (Bean.Email), Bean);
+      Outcome := To_Unbounded_String ("success");
+   end Send;
 
    --  ------------------------------
-   --  This bean provides some methods that can be used in a Method_Expression
+   --  Save the member information.
    --  ------------------------------
    overriding
-   function Get_Method_Bindings (From : in Member_Bean)
-                                 return Util.Beans.Methods.Method_Binding_Array_Access is
-      pragma Unreferenced (From);
+   procedure Save (Bean    : in out Member_Bean;
+                   Outcome : in out Ada.Strings.Unbounded.Unbounded_String) is
    begin
-      return Member_Bean_Binding'Access;
-   end Get_Method_Bindings;
+      Bean.Module.Save (Bean.Id, To_String (Bean.Key), Bean);
+      Outcome := To_Unbounded_String ("saved");
+
+   exception
+      when ADO.Objects.NOT_FOUND =>
+         Invalid_Secure_Key (Outcome);
+   end Save;
+
+   --  ------------------------------
+   --  Validate the member information.
+   --  ------------------------------
+   overriding
+   procedure Load (Bean    : in out Member_Bean;
+                   Outcome : in out Ada.Strings.Unbounded.Unbounded_String) is
+   begin
+      if Bean.Id > 0 then
+         Bean.Module.Load (Bean.Id, Bean, Bean.History);
+      else
+         Bean.Module.Validate_Key (To_String (Bean.Key), Bean);
+      end if;
+      Bean.Email := Bean.Get_Email.Get_Email;
+      Outcome := To_Unbounded_String ("loaded");
+
+   exception
+      when ADO.Objects.NOT_FOUND =>
+         Invalid_Secure_Key (Outcome);
+   end Load;
+
+   --  ------------------------------
+   --  Validate the member information.
+   --  ------------------------------
+   overriding
+   procedure Validate (Bean    : in out Member_Bean;
+                       Outcome : in out Ada.Strings.Unbounded.Unbounded_String) is
+   begin
+      Bean.Module.Validate_Key (To_String (Bean.Key), Bean);
+      Bean.Email := Bean.Get_Email.Get_Email;
+
+   exception
+      when ADO.Objects.NOT_FOUND =>
+         Outcome := To_Unbounded_String ("failure");
+         Messages.Factory.Add_Field_Message ("key", "members.secure_key_invalid_error",
+                                             Messages.ERROR);
+   end Validate;
+
+   --  ------------------------------
+   --  Register the member information.
+   --  ------------------------------
+   overriding
+   procedure Register (Bean    : in out Member_Bean;
+                       Outcome : in out Ada.Strings.Unbounded.Unbounded_String) is
+   begin
+      Bean.Module.Register (Bean);
+
+   exception
+      when ADO.Objects.NOT_FOUND =>
+         Invalid_Secure_Key (Outcome);
+   end Register;
+
+   --  ------------------------------
+   --  Update the member after receiving the contribution.
+   --  ------------------------------
+   overriding
+   procedure Save_Payment (Bean    : in out Member_Bean;
+                           Outcome : in out Ada.Strings.Unbounded.Unbounded_String) is
+   begin
+      Bean.Module.Save_Payment (Bean.Id, Bean);
+      Outcome := To_Unbounded_String ("saved");
+   end Save_Payment;
 
    --  ------------------------------
    --  Create the Member_Bean bean instance.
@@ -82,7 +173,62 @@ package body Adafr.Members.Beans is
       Object : constant Member_Bean_Access := new Member_Bean;
    begin
       Object.Module := Module;
+      Object.History_Bean := Object.History'Access;
       return Object.all'Access;
    end Create_Member_Bean;
+
+   --  ------------------------------
+   --  Get the value identified by the name.
+   --  ------------------------------
+   overriding
+   function Get_Value (From : in Member_List_Bean;
+                       Name : in String) return Util.Beans.Objects.Object is
+   begin
+      if Name = "members" then
+         return Util.Beans.Objects.To_Object (Value   => From.Members_Bean,
+                                              Storage => Util.Beans.Objects.STATIC);
+
+      elsif Name = "page" then
+         return Util.Beans.Objects.To_Object (From.Page);
+
+      elsif Name = "count" then
+         return Util.Beans.Objects.To_Object (From.Count);
+
+      else
+         return From.Members.Get_Value (Name);
+      end if;
+   end Get_Value;
+
+   --  ------------------------------
+   --  Load the list of members.
+   --  ------------------------------
+   overriding
+   procedure Load (List    : in out Member_List_Bean;
+                   Outcome : in out Ada.Strings.Unbounded.Unbounded_String) is
+      Ctx     : constant ASC.Service_Context_Access := ASC.Current;
+      Session : ADO.Sessions.Session := ASC.Get_Session (Ctx);
+      Query   : ADO.Queries.Context;
+   begin
+      Query.Set_Query (Adafr.Members.Models.Query_Adafr_Member_List);
+      Query.Bind_Param ("status", Integer (Models.Status_Type'Pos (List.Status)));
+      Adafr.Members.Models.List (List.Members_Bean.all, Session, Query);
+      Outcome := To_Unbounded_String ("success");
+   end Load;
+
+   --  ------------------------------
+   --  Create the Members_List_Bean bean instance.
+   --  ------------------------------
+   function Create_Member_List_Bean (Module : in Adafr.Members.Modules.Member_Module_Access)
+                                     return Util.Beans.Basic.Readonly_Bean_Access is
+      Object : constant Member_List_Bean_Access := new Member_List_Bean;
+   begin
+      Object.Module := Module;
+      Object.Page_Size := 20;
+      Object.Page := 1;
+      Object.Count := 0;
+      Object.Status := Adafr.Members.Models.MEMBER_ADA_FRANCE;
+      Object.Members_Bean := Object.Members'Access;
+      return Object.all'Access;
+   end Create_Member_List_Bean;
 
 end Adafr.Members.Beans;
